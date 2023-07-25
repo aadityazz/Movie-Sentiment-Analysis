3b91418e6072d5c4b453d3736ab58a81b28a0b87
Your Name <you@example.com>
2023-07-26 00:37:29 +0530

    first commit

--- app.py
+++ app.py
@@ -300,4 +300,4 @@ def predict_endpoint():
 
 if __name__ == '__main__':
     # Run the Flask app on the local development server
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0")
