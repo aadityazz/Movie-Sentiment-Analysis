24f3314246b57b7449faec681ebf0faf7a6fd7e4
Aditya Gupta <67819043+aadityazz@users.noreply.github.com>
2023-07-26 01:24:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -302,4 +302,4 @@ def predict_endpoint():
 
 if __name__ == '__main__':
     # Run the Flask app on the local development server
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=8080)
